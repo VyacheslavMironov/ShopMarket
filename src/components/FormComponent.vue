d3eef13b4fa227e29f948bebe95aa8382d25d025
VuacheslavMironov <vuacheslav.mir@gmail.com>
2023-05-16 14:33:34 +0700

    Сделал модели данных

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -3,32 +3,81 @@
         <form>
             <div class="mb-3">
                 <label class="form-label">Введите имя</label>
-                <input type="text" class="form-control" >
+                <input 
+                    type="text" 
+                    class="form-control"
+                    v-model="first_name"
+                >
             </div>
             <div class="mb-3">
                 <label class="form-label">Введите фамилию</label>
-                <input type="text" class="form-control" >
+                <input 
+                    type="text" 
+                    class="form-control"
+                    v-model="last_name"
+                >
             </div>
             <div class="mb-3">
                 <label class="form-label">Введите E-mail</label>
-                <input type="email" class="form-control" >
+                <input 
+                    type="email" 
+                    class="form-control"
+                    v-model="email"
+                >
             </div>
             <div class="mb-3">
                 <label class="form-label">Введите номер телефона</label>
-                <input type="text" class="form-control" >
+                <input
+                    type="text" 
+                    class="form-control"
+                    v-model="phone"
+                >
             </div>
             <div class="mb-3">
                 <label class="form-label">Введите роль</label>
-                <select class="form-select" aria-label="Default select example">
+                <select 
+                    class="form-select" 
+                    aria-label="Default select example"
+                    v-model="role"
+                >
                     <option value="Продавец">Продавец</option>
                     <option value="Покупатель">Покупатель</option>
                 </select>
             </div>
             <div class="mb-3">
                 <label class="form-label">Введите пароль</label>
-                <input type="password" class="form-control" >
+                <input
+                    type="password"
+                    class="form-control"
+                    v-model="password"
+                >
             </div>
-            <button type="submit" class="btn btn-primary">Отправить</button>
+            <button 
+                type="submit" 
+                class="btn btn-primary"
+                v-on:click="add"
+            >Отправить</button>
         </form>
     </div>
 </template>
+
+<script>
+    export default{
+        data(){
+            return {
+                first_name: '',
+                last_name: '',
+                email: '',
+                phone: '',
+                role: '',
+                password: '',
+            }
+        },
+        methods: {
+            add: function()
+            {
+                // проверить что данные записаны в модел данных
+            }
+        }
+    }
+</script>
